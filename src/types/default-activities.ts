4cbbd7ca777b42b346a10da812968ec5a99ac647
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-08-18 12:36:56 -0400

    create types for activities

---
+++ src/types/default-activities.ts
@@ -0,0 +1,61 @@
+export interface IPredefinedActivityCard {
+  name: string;
+  type: string;
+  participants: number;
+  price: number;
+  accessibility: number;
+  id: string;
+  imageId: string;
+  blur_hash: string;
+  urls: Urls;
+  user: User;
+  averageRating: number;
+  reviews: number;
+}
+
+export interface Urls {
+  full: string;
+  thumb: string;
+  regular: string;
+}
+
+export interface User {
+  name: string;
+  username: string;
+  links: Links;
+  profile_image: ProfileImage;
+}
+
+export interface Links {
+  html: string;
+}
+
+export interface ProfileImage {
+  medium: string;
+}
+
+export type IPredefinedActivityIDArray = {
+  rating: number;
+  id: string;
+};
+
+export enum ActivityType {
+  Education = "education",
+  Recreational = "recreational",
+  Social = "social",
+  DIY = "diy",
+  Charity = "charity",
+  Cooking = "cooking",
+  Relaxation = "relaxation",
+  Music = "music",
+  Busywork = "busywork",
+}
+
+export interface IPredefinedActivity extends IPredefinedActivityCard {
+  description: string;
+  listOfLinks: ActivityLink;
+}
+
+interface ActivityLink {
+  url: string;
+}

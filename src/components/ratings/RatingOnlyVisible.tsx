d6127d5c536574239fa307a8097af4264df1ace9
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-08-21 15:21:30 -0400

    create only visible rating component

---
+++ src/components/ratings/RatingOnlyVisible.tsx
@@ -0,0 +1,38 @@
+import { FaStar } from "react-icons/fa";
+
+const RatingOnlyVisible = ({
+  averageRating,
+  totalRatings,
+}: {
+  averageRating: number;
+  totalRatings: number;
+}) => {
+  return (
+    <div className="flex items-center">
+      {[...Array(5)].map((_, index) => {
+        const givenRating = index + 1;
+
+        return (
+          <label
+            key={givenRating}
+            className="duration-300"
+          >
+            <FaStar
+              className="text-lg my-2 duration-200"
+              color={givenRating <= averageRating ? "#fdc500" : "rgb(192,192,192)"}
+            />
+          </label>
+        );
+      })}
+      {totalRatings === 0 ? (
+        <p className="ml-2 text-sm text-gray-600">There are no ratings yet</p>
+      ) : (
+        <p className="ml-2 text-sm text-gray-600">
+          {averageRating.toFixed(1)} ({totalRatings} {totalRatings === 1 ? "rating" : "ratings"})
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default RatingOnlyVisible;

6f35f205d884d15559c2802ed94366d514386669
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-08-23 22:06:34 -0400

    obtain current user uid

--- src/components/layout/RootLayout.tsx
+++ src/components/layout/RootLayout.tsx
@@ -1,14 +1,18 @@
 import Sidebar from "./Sidebar";
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
+import { useAuth } from "../../hooks/useAuth";
 
 
 export default function RootLayout() {
+
+  const { currentUser} = useAuth();
+
   return (
     <>
       <main className="block md:grid md:grid-flow-col grid-cols-5 m-auto">
         <div className="col-span-1 hidden md:block">
-          <Sidebar />
+          <Sidebar uid={currentUser?.uid!} />
         </div>
        <Header/>
         <div className="col-span-5 flex container p-8">

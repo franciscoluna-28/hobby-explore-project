6603f9ad7ebe10352e94300c81852c5f961d0dd3
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-08-25 17:13:28 -0400

    add error screen and skeletons

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,34 +1,63 @@
 import ActivityCard from "../components/activities/ActivityCard";
-import LoadingSpinner from "../components/ui/loading-spinner";
+import ErrorScreen from "../components/errors/error-screen";
+import RecommendedActivitiesSkeleton from "../components/skeleton/recommended-activities-skeleton";
+import { Button } from "../components/ui/button";
 import { useRecommendedDefaultActivities } from "../features/default-activities/api/use-get-random-default-activities";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function Home() {
+  // Use the hook to get the default activities
   const {
     error,
     data: recommendedActivities,
     status,
+    refetch,
   } = useRecommendedDefaultActivities();
 
+  // The API data is loading, we render a skeleton
   if (status === "loading") {
-    return <LoadingSpinner />;
+    return <RecommendedActivitiesSkeleton numberOfActivities={3} />;
   }
 
+  // The data didn't load correctly, we render the error screen
   if (error && status === "error") {
-    return <p>Error!</p>;
+    return (
+      <ErrorScreen
+        message="Ooops. We weren't able to retrieve activities at the moment."
+        children={
+          <Button
+            className="bg-transparent text-accent border-2 border-accent hover:bg-accent hover:text-white duration-200"
+            onClick={() => refetch()}
+          >
+            Try Again
+          </Button>
+        }
+      />
+    );
   }
-
-  console.log(recommendedActivities)
 
   return (
-    <div>
-      <h1 className="text-accent w-full text-3xl lg:text-4xl font-bold">Activities for you</h1>
+    // Entry animation
+    <AnimatePresence>
+      <div>
+        <h1 className="text-accent w-full text-3xl lg:text-4xl font-bold">
+          Activities for you
+        </h1>
 
-      <ul className="columns-1 sm:columns-2 w-full space-y-8 mt-8 lg:columns-3">
-        {recommendedActivities &&
-          recommendedActivities.map((activity) => (
-            <ActivityCard {...activity} key={activity.id} />
-          ))}
-      </ul>
-    </div>
+        <ul className="columns-1 sm:columns-2 w-full space-y-8 mt-8 lg:columns-3">
+          {recommendedActivities &&
+            recommendedActivities.map((activity) => (
+              <motion.li
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                key={activity.id}
+              >
+                <ActivityCard {...activity} />
+              </motion.li>
+            ))}
+        </ul>
+      </div>
+    </AnimatePresence>
   );
 }

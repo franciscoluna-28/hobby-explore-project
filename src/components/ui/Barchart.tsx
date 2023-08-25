f4738c479b12a615b4501917d724e5816ca33514
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-08-25 17:12:01 -0400

    handle empty state for a better UX

--- src/components/ui/Barchart.tsx
+++ src/components/ui/Barchart.tsx
@@ -9,13 +9,26 @@ import {
     ResponsiveContainer,
   } from "recharts";
 import { FavoriteCategories } from "../../pages/Profile";
+import { Link } from "react-router-dom";
   
 
   interface BarChartSectionProps {
     data: FavoriteCategories;
   }
+
   
   export default function BarChartSection({ data }: BarChartSectionProps) {
+
+
+    
+  if(data.length === 0 || !data) {
+    return(
+      <div className="flex flex-col justify-center items-center">
+    <div className="my-8 text-sm max-w-xl m-auto text-center leading-loose">You haven't saved activities yet! In order to see your stats, you need to save activities! <Link className="underline font-bold underline-offset-2" to="/home">Start here</Link></div>
+   
+    </div> 
+    )}
+
     return (
       <ResponsiveContainer width="100%" height={300} className="mt-4">
         <BarChart data={data}>

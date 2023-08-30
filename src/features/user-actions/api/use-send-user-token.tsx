4ac36802b122e2f4d79309031357a1e9239c0531
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-08-29 20:37:20 -0400

    test user token request

--- src/features/user-actions/api/use-send-user-token.tsx
+++ src/features/user-actions/api/use-send-user-token.tsx
@@ -1,6 +1,9 @@
 import { axios } from "../../axios";
 
+
+
 export function registerUserToken(uid: string, token: string) {
+  console.log(uid, token);
   return axios.post(
     `/user/register-user-token/${uid}`,
     {

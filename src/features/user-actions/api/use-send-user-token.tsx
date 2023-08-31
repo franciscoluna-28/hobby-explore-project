47f48f3512762c1d58dc78af7b8865851682ecb0
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-08-31 11:52:04 -0400

    update route

--- src/features/user-actions/api/use-send-user-token.tsx
+++ src/features/user-actions/api/use-send-user-token.tsx
@@ -1,18 +1,21 @@
-import { axios } from "../../axios";
+import axios from "axios";
 
-
-
-export function registerUserToken(uid: string, token: string) {
-  console.log(uid, token);
-  return axios.post(
-    `/user/register-user-token/${uid}`,
-    {
-      bearedToken: token,
-    },
-    {
-      headers: {
-        Authorization: `Bearer ${token}`,
+export async function registerUserToken(uid: string, token: string) {
+  try {
+    const response = await axios.post(
+      `https://hobby-explore-backend.onrender.com/api/user/register-user-token/${uid}`,
+      {
+        bearedToken: token,
       },
-    }
-  );
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    return response.data;
+  } catch (error) {
+    throw new Error("Error registering user token");
+  }
 }

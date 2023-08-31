2523684a7e6f8307dd5073d38958623457f5f781
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-08-31 11:52:32 -0400

    create hook to handle global axios instance

--- src/features/axios.tsx
+++ src/features/axios.tsx
@@ -1,27 +1,34 @@
 import Axios from "axios";
 import { AxiosRequestHeaders } from "axios";
+import { useEffect } from "react";
 
 interface AxiosRequestConfig {
   headers: AxiosRequestHeaders;
 }
 
-// Creating global axios instance
 export const axios = Axios.create({
   baseURL: import.meta.env.VITE_BASE_API_URL,
 });
 
-// Getting an existent instance of the user accessToken
-const userToken = sessionStorage.getItem("accessToken");
-console.log("user token for requests is " + userToken); 
+// Creating global axios instance
+export function useGlobalAxiosInstance(token: string) {
+  useEffect(() => {
+    function authRequestInterceptor(config: AxiosRequestConfig) {
+      console.log("token used for requests is " + token);
+      if (token) {
+        console.log("global token is ", token);
+        config.headers.authorization = `Bearer ${token}`;
+      }
+      return config;
+    }
 
-// And intercepting it into the API requests within the global Axios object
-function authRequestInterceptor(config: AxiosRequestConfig) {
-  if (userToken) {
-    console.log("global token is + " ,userToken)
-    config.headers.authorization = `Bearer ${userToken}`;
-  }
-  return config;
+    const requestInterceptor = axios.interceptors.request.use(
+      authRequestInterceptor
+    );
+
+    return () => {
+      // Clean up by ejecting the interceptor when the component unmounts
+      axios.interceptors.request.eject(requestInterceptor);
+    };
+  }, [token]); // Run this effect only once, when the component mounts
 }
-
-// Use of the interceptor in all the requests
-axios.interceptors.request.use(authRequestInterceptor);

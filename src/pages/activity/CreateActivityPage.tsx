e32c8bf8ccdbe2919271006c6bedf3763e203f95
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-08-31 17:26:00 -0400

    add message to page

--- src/pages/activity/CreateActivityPage.tsx
+++ src/pages/activity/CreateActivityPage.tsx
@@ -2,6 +2,7 @@ export default function CreateActivity() {
     return(
         <div>
             <h1>Get Started by creating your own activity here!</h1>
+            <p>WIP. I'm gonna start making the business logic to create a custom activity in the backend this weekend (31/08/23)</p>
         </div>
     )
 }

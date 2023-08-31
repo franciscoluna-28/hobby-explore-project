c4f4889ab9c80c3bbcf7fe3469558c5afbf0847c
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-08-31 17:22:54 -0400

    add note

--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -7,11 +7,11 @@ import Logo from "../../assets/logo.png"
 // Besides the auth form this includes a small carousel of quotes as a detail
 export default function Register() {
   return (
-    <main className="">
-      <section className="flex flex-row-reverse overflow-hidden">
-        <div className="flex flex-col w-full p-8">
-          <div className="flex flex-col m-auto min-w-full xl:max-w-7xl gap-2">
-          <img className="w-36 m-auto h-20" src={Logo}></img>
+    <main className="flex justify-center h-screen lg:h-auto lg:block">
+      <section className="flex flex-row-reverse overflow-hidden w-full">
+        <div className="flex flex-col w-full p-6 md:p-8">
+          <div className="flex flex-col m-auto min-w-full xl:max-w-7xl gap-1 md:gap-2">
+          <img className="w-44 m-auto h-24" src={Logo}></img>
             <h1 className="text-center text-4xl leading-tight font-semibold text-accent">
               Welcome!
             </h1>

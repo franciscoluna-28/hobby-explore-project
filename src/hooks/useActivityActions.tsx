a005f0ba5285ab699197195d7994891572351226
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-09-03 12:30:30 -0400

    create hook to handle with the actions of the acts

--- src/hooks/useActivityActions.tsx
+++ src/hooks/useActivityActions.tsx
@@ -9,11 +9,13 @@ export function useActivityActions(id: string) {
   const { uid } = currentUser!;
   const { toast } = useToast();
 
+
   const { data, refetch } = useIsActivitySavedByUser(uid, id);
   const addActivityToUserMutation = useAddActivityToUser();
 
   const [isSaved, setIsSaved] = useState(data?.valueOf());
 
+  // TODO: This should be in a store tbh
   const saveActivity = async (id: string) => {
     try {
       console.log(id);
@@ -25,13 +27,18 @@ export function useActivityActions(id: string) {
       toast({
         title: "✅ Activity Saved Successfully",
         description: "You've added a new activity to your list!",
-      })
-      console.log(isSaved)
+      });
+      console.log(isSaved);
     } catch (error) {
       console.error("Error saving activity:", error);
     }
   };
 
+  // TODO: Use a store to remove this dependency and do less API requests
+  // TODO: What am I even doing?
+  // TODO: I think I should stat selling minecraft mods instead of doing web applications
+  // TODO: Quit this fullstack thing (But wait I have to buy a Porsche with it lol)
+  // With this we can create updates in real time instead of hardcoding the useEffect LOL
   useEffect(() => {
     setIsSaved(data?.valueOf()); // Update the saved state on data change
   }, [data]);

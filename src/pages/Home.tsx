693d3f795204f315222e5fa44f31c946910ab38e
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-09-03 12:28:44 -0400

    create logic to handle filtering using a hook

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,30 @@
-import ActivityCard from "../components/activities/ActivityCard";
-import ErrorScreen from "../components/errors/error-screen";
-import RecommendedActivitiesSkeleton from "../components/skeleton/recommended-activities-skeleton";
 import { Button } from "../components/ui/button";
-import { useRecommendedDefaultActivities } from "../features/default-activities/api/use-get-random-default-activities";
 import { AnimatePresence, motion } from "framer-motion";
+import { useActivitiesManager } from "../hooks/useActivityManager";
+import RecommendedActivitiesSkeleton from "../components/skeleton/recommended-activities-skeleton";
+import ErrorScreen from "../components/errors/error-screen";
+import CategoryFilters from "../components/activities/ActivityCategoryFilters";
+import { IPredefinedActivity } from "../types/default-activities";
+import ActivityCard from "../components/activities/ActivityCard";
 
 export default function Home() {
-  // Use the hook to get the default activities
   const {
-    error,
-    data: recommendedActivities,
+    selectedCategory,
+    handleCategoryFilter,
+    recommendedActivities,
     status,
+    error,
     refetch,
-  } = useRecommendedDefaultActivities();
-
+  } = useActivitiesManager();
 
   console.log(recommendedActivities)
-  // The API data is loading, we render a skeleton
+
+  // The API data is loading, render a skeleton
   if (status === "loading") {
     return <RecommendedActivitiesSkeleton numberOfActivities={6} />;
   }
 
-  // The data didn't load correctly, we render the error screen
+  // The data didn't load correctly, render the error screen
   if (error && status === "error") {
     return (
       <ErrorScreen
@@ -39,16 +42,16 @@ export default function Home() {
   }
 
   return (
-    // Entry animation
     <AnimatePresence>
       <div>
-        <h1 className="text-accent w-full text-3xl lg:text-4xl font-bold">
-          Activities for you
-        </h1>
-
+        <h1 className="text-accent font-bold text-4xl">Discover Activities</h1>
+        <CategoryFilters
+          handleCategoryFilter={handleCategoryFilter}
+          selectedCategory={selectedCategory}
+        />
         <ul className="columns-1 sm:columns-2 w-full space-y-8 mt-8 lg:columns-3">
           {recommendedActivities &&
-            recommendedActivities.map((activity) => (
+            recommendedActivities.map((activity: IPredefinedActivity) => (
               <motion.li
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}

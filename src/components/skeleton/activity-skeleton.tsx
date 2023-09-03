77ec088c9fb13331ceb73f4dd02a3522d4a99d40
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-09-03 12:32:14 -0400

    make skeleton fully responsive

--- src/components/skeleton/activity-skeleton.tsx
+++ src/components/skeleton/activity-skeleton.tsx
@@ -1,24 +1,22 @@
 import { Skeleton } from "../ui/skeleton";
 
-// Skeleton based on the activity card
 export default function ActivitySkeleton() {
-    return (
-        <Skeleton className="bg-slate-100 w-80 h-full flex flex-col">
-            <div className="flex flex-col gap-4">
+  return (
+    <Skeleton className="bg-slate-100 w-full h-full flex flex-col">
+      <div className="flex flex-col gap-4">
+        <div className="relative">
+          <Skeleton className="w-full max-w-screen-xl h-48 m-auto bg-slate-300" />
+          <Skeleton className="w-16 h-16 rounded-full bg-slate-400 absolute top-0 left-4 my-4" />
+        </div>
 
-                <div className="relative">
-                    <Skeleton className="w-80 h-48 m-auto bg-slate-300" />
-                    <Skeleton className="w-16 h-16 rounded-full bg-slate-400 absolute top-0 left-4 my-4 py-2" />
-                </div>
-
-                <div className="mx-4 my-2">
-                    <Skeleton className="w-72 h-8 bg-slate-300" />
-                    <Skeleton className="w-64 mt-4 h-4 bg-slate-300" />
-                    <Skeleton className="w-24 mt-4 h-4 bg-slate-300" />
-                    <Skeleton className="w-32 mt-4 h-8 bg-slate-300" />
-                    <Skeleton className="w-48 mt-4 h-8 bg-slate-300" />
-                </div>
-            </div>
-        </Skeleton>
-    );
+        <div className="mx-4 my-2">
+          <Skeleton className="w-full max-w-screen-xl h-8 bg-slate-300" />
+          <Skeleton className="w-full max-w-screen-xl mt-4 h-4 bg-slate-300" />
+          <Skeleton className="w-20 max-w-screen-xl mt-4 h-4 bg-slate-300" />
+          <Skeleton className="w-24 max-w-screen-xl mt-4 h-8 bg-slate-300" />
+          <Skeleton className="w-40 max-w-screen-xl mt-4 mb-2 h-8 bg-slate-300" />
+        </div>
+      </div>
+    </Skeleton>
+  );
 }

ed5a126e1c38d7ecb1b22c49469999a54e2f6947
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-09-03 12:32:57 -0400

    add vertical centering

--- src/components/errors/error-screen.tsx
+++ src/components/errors/error-screen.tsx
@@ -8,7 +8,7 @@ type Props = {
 export default function ErrorScreen({ message, children }: Props) {
     return (
         
-        <div className="m-auto mt-16 max-h-full overflow-hidden flex items-center justify-center h-full">
+        <div className="m-auto max-h-full overflow-hidden flex items-center justify-center h-screen">
             <div className="flex flex-col items-center">
                 <img className="max-w-48 md:w-64 m-auto md:h-64 h-48" src={Bird} alt="Bird" />
                 <span className="text-sm md:text-lg text-center my-4">{message}</span>
